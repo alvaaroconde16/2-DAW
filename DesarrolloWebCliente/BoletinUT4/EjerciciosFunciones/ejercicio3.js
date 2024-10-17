9d99ed7ed8bc74687fadb6dfd6abd840c7278b48
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2024-10-17 13:26:40 +0200

    Ejercicios funciones

--- DesarrolloWebCliente/BoletinUT4/EjerciciosFunciones/ejercicio3.js
+++ DesarrolloWebCliente/BoletinUT4/EjerciciosFunciones/ejercicio3.js
@@ -1,3 +1,6 @@
+/*Realizar un programa que calcule el número de cifras de un número. Deberá implementarse una función numCifras(numero) 
+que devuelva el número de cifras del mismo. Utiliza el operador spread.*/
+
 var numero = 45464107;
 
 function numCifras(numero){
---
+++ DesarrolloWebCliente/BoletinUT4/EjerciciosFunciones/ejercicio6.js
@@ -0,0 +1,25 @@
+/*Hacer un programa que sume todos los parámetros pasados como argumentos de entrada en la llamada. El número de argumentos de entrada es desconocido.*/
+
+function sumParam(...restParams){
+    let suma = 0;
+
+    for (let i = 0; i < restParams.length; i++) {
+        suma += restParams[i]
+    }
+
+    return suma
+
+}
+
+let sumParam = (...restParams) => {
+    let suma = 0;
+
+    for (let i = 0; i < restParams.length; i++) {
+        suma += restParams[i]
+    }
+
+    return suma
+
+    }
+
+alert(sumParam(2,3,5))
