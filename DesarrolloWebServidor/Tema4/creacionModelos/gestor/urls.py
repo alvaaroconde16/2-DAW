8c505d737c4b209f4b0c817633c025419a8602e5
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2024-10-24 11:13:59 +0200

    Creacion Modelos con URLs, Views y QuerySet

--- DesarrolloWebServidor/Tema4/creacionModelos/gestor/urls.py
+++ DesarrolloWebServidor/Tema4/creacionModelos/gestor/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('proyectos/listar', views.listar_proyectos, name='listar_proyectos'),
     path('proyectos/<int:id_proyecto>', views.listar_tareas, name='listar_tareas'),
     path('tarea/<int:id_tarea>/usuarios', views.listar_usuarios_tarea, name='listar_usuarios_tarea'),
-    path('tarea/observaciones/<str:observacion>', views.tareas_con_observaciones, name='tareas_con_observaciones'),
+    path('proyectos/<int:id_proyecto>/tarea/<str:observacion>', views.tareas_con_observaciones, name='tareas_con_observaciones'),
+    path('tarea/completadas/<int:anyo_inicio>/<int:anyo_fin>', views.tareas_completadas, name='tareas_completadas'),
+    path('ultimo-usuario-comentar/<int:proyecto>', views.ultimo_usuario_comentar, name='ultimo_usuario_comentar'),
 ]
--- DesarrolloWebServidor/Tema4/creacionModelos/gestor/views.py
+++ DesarrolloWebServidor/Tema4/creacionModelos/gestor/views.py
@@ -5,6 +5,7 @@ from .models import Proyecto
 from .models import Etiqueta
 from .models import AsignacionTarea
 from .models import Comentario
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -18,25 +19,46 @@ def listar_proyectos(request):
     return render(request, 'proyecto/lista.html', {"proyecto_mostrar":proyecto})
 
 
+
 #Vamos a mostrar todas las tareas que tiene un proceso ordenandolos por la fecha de creación de forma descendente
 def listar_tareas(request, id_proyecto):
-    tarea = Tarea.objects.select_related("proyecto").select_related("creador").prefetch_related("usuarios").order_by('-fecha_creacion')
-    tarea = tarea.filter(proyecto_id=id_proyecto)
-    
+    tarea = Tarea.objects.select_related("proyecto", "creador").prefetch_related("usuarios").order_by('-fecha_creacion')
+    tarea = tarea.filter(proyecto=id_proyecto)
+    0
     return render(request, 'proyecto/tareas.html', {'tarea_mostrar':tarea})
+
 
 
 #Vamos a mostrar todos los usuarios que están asignados a una tarea ordenandolos por fecha de asignacion de forma ascendente
 def listar_usuarios_tarea(request, id_tarea):
-    asignacion = AsignacionTarea.objects.select_related("usuario").select_related("tarea")
+    asignacion = AsignacionTarea.objects.select_related("usuario", "tarea")
     asignacion = asignacion.filter(tarea_id=id_tarea).order_by('fecha_asignacion')
     
     return render(request, 'tarea/usuarios.html', {'asignacion_mostrar':asignacion})
 
 
-#Mostramos todas las tareas que tengan un texto concreto en observaciones
-def tareas_con_observaciones(request, observacion):
-    tarea = AsignacionTarea.objects.select_related("usuario").select_related("tarea")
-    tarea = tarea.filter(observaciones__contains=observacion)
 
-    return render(request, 'proyecto/tareas.html', {'tarea_mostrar':tarea})
+#Mostramos todas las tareas que tengan un texto concreto en observaciones
+def tareas_con_observaciones(request, id_proyecto ,observacion):
+    asignacionesTareas = AsignacionTarea.objects.select_related("usuario", "tarea")
+    #tarea = Tarea.objects.select_related("proyecto").select_related("creador").prefetch_related("usuarios")
+    asignacionesTareas = asignacionesTareas.filter(tarea__proyecto_id=id_proyecto).filter(observaciones__contains=observacion)
+
+    return render(request, 'tarea/observaciones.html', {'asignaciones':asignacionesTareas})
+
+
+
+#Vamosa mostrar todas las tareas que estén realizadas entre dos años y que su estado sea: 'Completada'
+def tareas_completadas(request, anyo_inicio, anyo_fin):   
+    tareas = Tarea.objects.select_related("proyecto", "creador").prefetch_related("usuarios")
+    tareas = tareas.filter(estado='Completada', fecha_creacion__year__range=[anyo_inicio, anyo_fin])
+    
+    return render(request, 'tarea/completadas.html', {'completadas':tareas})
+
+
+
+#Vamos a mostrar el úlitmo usuario que comentó en una tarea de un proyecto especifico
+def ultimo_usuario_comentar(request, id_proyecto):
+    #comentario = Tarea.objects.select_related("administrador").prefetch_related("colaboradores")
+    comentario = Tarea.objects.select_related("proyecto", "creador").prefetch_related("usuarios")
+    comentario = comentario.filter()
