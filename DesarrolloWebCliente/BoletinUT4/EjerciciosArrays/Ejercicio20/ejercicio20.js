1e2ae0d9bb098a33ac091bf0bed916366e0e0f73
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2024-10-25 10:57:09 +0200

    Ejercicios Arrays

--- DesarrolloWebCliente/BoletinUT4/EjerciciosArrays/Ejercicio20/ejercicio20.js
+++ DesarrolloWebCliente/BoletinUT4/EjerciciosArrays/Ejercicio20/ejercicio20.js
@@ -7,7 +7,7 @@ var datos = new Array();
 var i = 0;
 
 while (cadena != "") {
-    let array = cadena.split(", ")
+    let array = cadena.split(",")
 
     datos[i] = array
 
@@ -16,7 +16,7 @@ while (cadena != "") {
 
 }*/
 
-datos = ["Alvaro", "Conde", "123A"], ["Luis", "Garcia", "123B"]
+datos = [["Alvaro", "Conde", "123A", "10/10/2000"], ["Luis", "Garcia", "123B", "20/12/2001"]]
 
 
 //Función para mostrar los datos que contiene nuestro array de datos
@@ -29,39 +29,56 @@ function mostrarDatos(){
 
 //Funcion flecha para conseguir los datos por apellidos o por DNI
 function buscarApellido(apellido){
-    let resultado = datos.filter((array) => array[1] == apellido)
+    let resultado = datos.filter((array) => array[1] == apellido);
+
+    return resultado;
+}
+
+
+function buscarDni(dni){
+    let resultado = datos.find((array) => array[2] == dni);
+
+    return resultado;
+}
+
+
+function buscarEdad(edad){
+    let resultado = datos.filter((array) => calcularEdad(array[3]) == edad);
 
     return resultado;
 }
 
 
 //Función para convertir la edad en años
-function calcularEdad(fechaNacimiento){
+function calcularEdad(edad){
     const hoy = new Date();
-    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
-    const mes = hoy.getMonth() - fechaNacimiento.getMonth();
-    const dia = hoy.getDate() - fechaNacimiento.getDate();
+    let año = edad.split("/")[2];
+    let fecha = hoy.getFullYear() - año; 
 
-    if (mes < 0 || (mes == 0 && dia < 0)) {
-        edad--;
-    }
-
-    return edad;
+    return fecha;
 }
 
 
 mostrarDatos();
 
-var opcion = prompt("Introduce opción para  buscar: " + 
-                      "1) Apellidos" + 
-                      "2) DNI" + 
-                      "3) Fecha Nacimiento")
+                      
+var opcion = prompt("Introduce opción para buscar:\n1) Apellidos\n2) DNI\n3) Fecha de Nacimiento\n"); 
+
 
 switch (opcion) {
     case "1":
         let apellido = prompt("Introduce el apellido por el que desea buscar: ")
-        console.log("Resultado de la búsqueda por apellido: ")
-        console.log(buscarApellido(apellido))
+        alert(buscarApellido(apellido))
+        break;
+
+    case "2":
+        let dni = prompt("Introduce el dni por el que desea buscar: ")
+        alert(buscarDni(dni))
+        break;
+
+    case "3":
+        let edad = prompt("Introduce la edad por la que desea buscar: ")
+        alert(buscarEdad(edad))
         break;
 
     default:
