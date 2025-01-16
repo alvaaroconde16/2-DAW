f9cfe74b74813185e6564b2ed0e6c98837df12ec
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2025-01-16 10:25:40 +0100

    Aplicacion Web

--- DesarrolloWebServidor/AplicacionWeb/mysite/settings.py
+++ DesarrolloWebServidor/AplicacionWeb/mysite/settings.py
@@ -27,9 +27,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
